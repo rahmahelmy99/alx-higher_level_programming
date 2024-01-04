6971a141af354d4127e977283b3b869e23faa371
rahma <helmyrahma4@gmail.com>
2024-01-03 18:33:10 -0800

    updated

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range(100):
+    if i < 10:
+        print("0{:d}".format(i), end=', ')
+    elif i >=10:
+        print("{:d}".format(i), end='\n' if i == 99 else ', ')
