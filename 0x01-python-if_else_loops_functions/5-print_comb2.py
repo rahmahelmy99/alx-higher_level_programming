1faef737a410fabb5760c6ce798b6224d8a05c4d
rahma <helmyrahma4@gmail.com>
2024-01-03 18:33:55 -0800

    updated

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,5 +2,5 @@
 for i in range(100):
     if i < 10:
         print("0{:d}".format(i), end=', ')
-    elif i >=10:
+    elif i > = 10:
         print("{:d}".format(i), end='\n' if i == 99 else ', ')
