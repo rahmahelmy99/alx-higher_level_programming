68bfa60cadfbe744e388fb0b79ccc7251e489581
rahma <helmyrahma4@gmail.com>
2024-01-09 19:06:25 -0800

    added

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string:
-        if i == c or i == C:
-            my_string.pop(i)
+    rest = ""
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' or my_string[i] != 'C':
+            rest += my_string[i]
 
-        new_str = my_string
-
+    return rest
